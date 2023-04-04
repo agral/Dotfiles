a8ac1f624a7f43065d428b70a1ac4a35197254c1
agral <adam@gralin.ski>
2023-04-04 23:06:14 +0200

    [nvim config] Add colorscheme module
    Adds colorscheme handling to my modular nvim config.
    Uses Tokyonight (folke/tokyonight.nvim) as default colorscheme.

---
+++ .config/nvim/lua/user/colorscheme.lua
@@ -0,0 +1,8 @@
+local colorscheme = "tokyonight"
+local is_good, _ = pcall(vim.cmd, string.format("colorscheme %s", colorscheme))
+
+if not is_good then
+  vim.cmd [[colorscheme default]]
+  local warning = string.format("Colorscheme %s not found. Using default colorscheme.", colorscheme)
+  vim.api.nvim_echo({{"[setup warning] ", "Error"}, {warning, "Normal"}}, true, {})
+end
