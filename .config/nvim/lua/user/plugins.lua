54ac5e568f221600dcd73be3361cd81fe78d24ae
agral <adam@gralin.ski>
2023-04-04 22:48:23 +0200

    [nvim config] Add plugins module

---
+++ .config/nvim/lua/user/plugins.lua
@@ -0,0 +1,47 @@
+-- Automatically install neovim-packer:
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = vim.fn.system({"git", "clone", "--depth", "1",
+      "https://github.com/wbthomason/packer.nvim",
+      install_path,
+  })
+  vim.api.nvim_echo({{"[setup info] ", "String"}, {"Packer installed. Please restart Neovim.", "Normal"}}, true, {})
+  vim.cmd([[packadd packer.nvim]])
+end
+
+local is_good, packer = pcall(require, "packer")
+if not is_good then
+  vim.api.nvim_echo({{"[setup error] ", "Error"}, {"packer module is missing", "Normal"}}, true, {})
+  return
+end
+
+packer.init({
+  display = {
+    open_fn = function()
+      return require("packer.util").float({border = "rounded"})
+    end,
+  },
+})
+
+-- Adds an autocommand to automatically sync packer on every save of plugins.lua
+-- (note: PackerSync == PackerUpdate + PackerCompile)
+vim.cmd([[
+  augroup packer_nvim_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+return require("packer").startup(
+  function(use)
+    use "wbthomason/packer.nvim" -- have Packer manage (update) itself
+
+    -- colorscheme:
+    use "folke/tokyonight.nvim"
+
+    -- Run PackerSync after cloning packer.nvim for the first time:
+    if PACKER_BOOTSTRAP then
+      require("packer").sync()
+    end
+  end
+)
