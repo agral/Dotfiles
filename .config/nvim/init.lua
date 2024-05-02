abfff0f1d90a47bd58ee5b7b934c85808450ab6d
agral <adam@gralin.ski>
2024-05-02 07:59:50 +0200

    [Neovim] Move autocmds to proper file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,26 +5,7 @@ end
 
 require("gral.options")
 require("gral.keymaps")
-
---[[ SECTION: AUTOCOMMANDS --]]
--- Disable auto-comment:
-vim.api.nvim_create_autocmd("BufEnter", {
-    callback = function()
-        vim.opt.formatoptions = { c = false, r = false, o = false }
-    end,
-})
-
--- Remember cursor position
-vim.api.nvim_create_autocmd("BufReadPost", {
-    callback = function()
-        -- Don't do this for gitcommit files:
-        if vim.bo.filetype == "gitcommit" then
-            return
-        end
-        vim.cmd([[if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]])
-    end,
-})
-
+require("gral.autocmds")
 
 --[[ SECTION: PLUGINS --]]
 -- Automatically install lazy.nvim plugin manager:
