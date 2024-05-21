d5bda74de4838c5f4d755820a8e369ddbab0abb1
agral <adam@gralin.ski>
2024-05-21 08:11:37 +0200

    [nvim] Overhaul plugins.lua; Packer to Lazy
    An almost complete rewrite of plugins.lua in order to switch
    from Packer to Lazy. Plugins configured in lua/plugins/
    are automatically included with the new setup.

--- .config/nvim/lua/gral/plugins.lua
+++ .config/nvim/lua/gral/plugins.lua
@@ -1,91 +1,40 @@
--- Automatically install neovim-packer:
-local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = vim.fn.system({"git", "clone", "--depth", "1",
-      "https://github.com/wbthomason/packer.nvim",
-      install_path,
-  })
-  vim.api.nvim_echo({{"[setup info] ", "String"}, {"Packer installed. Please restart Neovim.", "Normal"}}, true, {})
-  vim.cmd([[packadd packer.nvim]])
+-- Automatically install lazy.nvim plugin manager:
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazy_path) then
+    local lazy_addr = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system({"git", "clone", "--filter=blob:none", lazy_addr, "--branch=stable", lazy_path })
+    vim.api.nvim_echo({{"[setup info] ", "String"}, {"Lazy package manager installed. Please restart Neovim.", "Normal"}}, true, {})
 end
 
-local is_good, packer = pcall(require, "packer")
+vim.opt.rtp:prepend(lazy_path)
+
+local is_good, lazy_plugin = pcall(require, "lazy")
 if not is_good then
-  vim.api.nvim_echo({{"[setup error] ", "Error"}, {"packer module is missing", "Normal"}}, true, {})
+  vim.api.nvim_echo({{"[setup error] ", "Error"}, {"lazy.nvim module is missing", "Normal"}}, true, {})
   return
 end
 
-packer.init({
-  display = {
-    open_fn = function()
-      return require("packer.util").float({border = "rounded"})
-    end,
-  },
+lazy_plugin.setup("plugins", {
+    change_detection = {
+        enabled = true,
+        notify = false,
+    },
+    ui = {
+        -- if NF available, use empty array - it will be overridden. Otherwise define base Unicode icons:
+        icons = vim.g.have_nerd_font and {} or {
+            cmd = '⌘',
+            config = '🛠',
+            event = '📅',
+            ft = '📂',
+            init = '⚙',
+            keys = '🗝',
+            plugin = '🔌',
+            runtime = '💻',
+            require = '🌙',
+            source = '📄',
+            start = '🚀',
+            task = '📌',
+            lazy = '💤 ',
+        },
+    },
 })
-
--- Adds an autocommand to automatically sync packer on every save of plugins.lua
--- (note: PackerSync == PackerUpdate + PackerCompile)
-vim.cmd([[
-  augroup packer_nvim_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
-
-return require("packer").startup(
-  function(use)
-    use "wbthomason/packer.nvim" -- have Packer manage (update) itself
-
-    -- colorscheme:
-    use "folke/tokyonight.nvim"
-
-    -- Autocompletion: nvim-cmp
-    use "hrsh7th/nvim-cmp"
-    use "hrsh7th/cmp-buffer"
-    --use "hrsh7th/cmp-cmdline"
-    use "hrsh7th/cmp-path"
-    use "hrsh7th/cmp-nvim-lua"
-    use "hrsh7th/cmp-nvim-lsp"
-
-    -- LSP:
-    use "neovim/nvim-lspconfig"
-    use "williamboman/mason.nvim"
-    use "williamboman/mason-lspconfig.nvim"
-
-    -- Snippets:
-    use "L3MON4D3/LuaSnip" -- snippet completion engine
-    use "saadparwaiz1/cmp_luasnip"
-
-    -- Git:
-    use "lewis6991/gitsigns.nvim"
-
-    -- Telescope:
-    use {
-      "nvim-telescope/telescope.nvim",
-      requires = {
-        {"nvim-lua/plenary.nvim"},
-      },
-    }
-
-    -- Treesitter:
-    use {
-      "nvim-treesitter/nvim-treesitter",
-      run = ":TSUpdate",
-    }
-
-    -- Other plugins:
-    use "nvim-tree/nvim-tree.lua"
-    use {
-      "nvim-lualine/lualine.nvim",
-      requires = {
-        "nvim-tree/nvim-web-devicons",
-        opt = true,
-      },
-    }
-
-    -- Run PackerSync after cloning packer.nvim for the first time:
-    if PACKER_BOOTSTRAP then
-      require("packer").sync()
-    end
-  end
-)
