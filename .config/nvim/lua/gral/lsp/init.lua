eb9db5bb90ca6b3addec821bb93140c6bff4a8bd
agral <adam@gralin.ski>
2024-05-21 09:26:32 +0200

    [nvim] Remove legacy lsp configuration

--- .config/nvim/lua/gral/lsp/init.lua
+++
@@ -1,6 +0,0 @@
--- custom mason configuration in lua/user/lsp/mason.lua:
-require("gral.lsp.mason")
--- Note: this loads both mason and mason-lspconfig in the correct order.
-
--- custom handlers' configuration in lua/user/lsp/handlers.lua:
-require("gral.lsp.handlers").setup()
