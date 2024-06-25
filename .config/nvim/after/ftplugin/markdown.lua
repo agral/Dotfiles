f043b3e4c3be1cf24b438d94ed4f3f1c414aa625
agral <adam@gralin.ski>
2024-06-25 11:16:17 +0200

    [nvim] Spec keymaps for Glow plugin for md files
    Configures custom keymaps for running Glow.
    Either F5 or <Leader>p (preview) invokes Glow to render a preview
    of currently edited file.
    These mappings are applied only for Markdown files with the use
    of after/ftplugin mechanism.

---
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,10 @@
+vim.keymap.set("n", "<leader>p", "<cmd>Glow<CR>", {
+    desc = "Preview a Markdown file with glow plugin",
+    noremap = true,
+    silent = true,
+})
+vim.keymap.set("n", "<F5>", "<cmd>Glow<CR>", {
+    desc = "Preview a Markdown file with glow plugin",
+    noremap = true,
+    silent = true,
+})
